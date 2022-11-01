36f830722a66ea47f6157b85c2632fe9ee203c92
Kami0rn <74646193+Kami0rn@users.noreply.github.com>
2022-11-02 01:49:30 +0700

    Add files via upload

--- lab8_1.c
+++ lab8_1.c
@@ -2,11 +2,28 @@
 #include <string.h>
 int main (void)
 {
-    char name[20] ;
-    printf("Enter name> ");
-    scanf("%[^\n]s",name);
-    printf("%s",name);
+    char name[4][20]= {"G. washington","J. Adams","T. Jefferson","J. madison"};
+    char init[4][20],last[4][20];
+    for (int i = 0; i < 4; i++)
+    {
+        strncpy(init[i],name[i],3);
+        init[i][3]='\0';
+        
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        strncpy(last[i],&name[i][3],20);
+        //last[i][20]
+    }
+    printf("Initial First Name\t\tLast Name\n");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%19s %21s\n",init[i],last[i]);
+    }
+    
 
+     
+    
 
 
 
